64aab2c65bc0b62676dfb4749d96487101b2f216
Ursula Miller <ursula.miller@gmail.com>
2019-06-24 03:59:54 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,7 +1,12 @@
 # require modules here
+require 'yaml'
+require 'pry'
 
-def load_library
-  # code goes here
+emoticon_lib = YAML.load_file('lib/emoticons.yml')
+
+def load_library(YAML.load_file('lib/emoticons.yml'))
+binding.pry
+
 end
 
 def get_japanese_emoticon
