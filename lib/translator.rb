6ec6a1691b5fd40887e20685ac8c49a343e9c8a5
Ursula Miller <ursula.miller@gmail.com>
2019-06-24 04:20:17 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -7,13 +7,13 @@ hash = YAML.load(File.read('lib/emoticons.yml'))
 
 path = 'lib/emoticons.yml'
 
-hash_emote_kingdom = {:get_meaning => {}, :get_emoticon=>{}}
+
 
 def load_library(path)
   
   emoticon_lib = YAML.load_file(path)
   emoticon_lib.each do |key, values|
-    hash_emote_kingdom
+  hash_emote_kingdom = {:get_meaning => {}, :get_emoticon=>{}}
   end
   binding.pry
   hash_emote_kingdom
